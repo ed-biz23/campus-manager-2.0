100b9d571a8795117632d38af7a712bf45c24c5e
Edward Biswas <edbiswas@gmail.com>
2020-01-19 02:52:56 -0500

    updated env

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,10 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    use_env_variable: process.env.DB_URI
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   }
 };
