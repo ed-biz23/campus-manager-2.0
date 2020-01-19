1b89e0d9e748581be0df11530c8c30e61144ab7d
Edward Biswas <edbiswas@gmail.com>
2020-01-19 12:49:55 -0500

    should be good

--- server/models/index.js
+++ server/models/index.js
@@ -8,8 +8,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  console.log(process.env[config.use_env_variable]);
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
+  sequelize = new Sequelize(config.use_env_variable, {
     dialect: "postgres",
     protocol: "postgres"
   });
