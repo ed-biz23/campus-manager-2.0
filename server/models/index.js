787b26909a1ee2aabe325f0f4e70001f7703a1ca
Edward Biswas <edbiswas@gmail.com>
2020-01-19 12:40:45 -0500

    updated db url

--- server/models/index.js
+++ server/models/index.js
@@ -1,39 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const configJson = require("../config/config");
-
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
-
-const config = configJson[env];
-
-console.log("this is the environment: ", env);
-
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(`${__dirname}/../config/config.js`)[env];
+console.log(config);
 const db = {};
 
 let sequelize;
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres"
-  });
-} else if (config.environment === "production") {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "postgres",
-      dialectOption: {
-        ssl: true,
-        native: true
-      },
-      logging: true
-    }
-  );
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database,
@@ -44,11 +20,10 @@ if (process.env.DATABASE_URL) {
 }
 
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
+  .filter(
+    file =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
+  )
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
